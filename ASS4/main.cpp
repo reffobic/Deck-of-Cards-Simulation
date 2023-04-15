5dfd144118542455123a41f8370c667b49ce3ca3
Refai <Mohamedref11@gmail.com>
2023-04-15 19:56:02 +0400

    Initial Commit

---
+++ ASS4/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ASS4
+//
+//  Created by Mohamed El-Refai on 15/04/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
